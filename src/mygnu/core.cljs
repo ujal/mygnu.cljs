68d706b4feabca075144510384a4adaec4b5a506
Udschal Imanov <udschal.imanov@gmail.com>
2015-05-19 21:22:25 +0200

    bs2

--- src/mygnu/core.cljs
+++ src/mygnu/core.cljs
@@ -16,22 +16,13 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(def string-list [{:string "Interactive" :type :header-first}
-                  {:string "Design & Development" :type :header-second}
-
-                  {:string "ABOUT" :type :nav}
-                  {:string "PROJECTS":type :nav}
-                  {:string "TOOLS":type :nav}])
-
 (defonce app-state
   (atom {:mouse {:x 0 :y 0}
          :particle-list []}))
 
-(defn particle-list []
-  (om/ref-cursor (:particle-list (om/root-cursor app-state))))
-
-(defn new-particle []
-  {:char nil
+(defn new-particle [s t]
+  {:string s
+   :type t
    :x 0
    :y 0
    :vx 0
@@ -42,50 +33,41 @@
    :origin-x nil
    :origin-y nil})
 
+(def string-list
+  [{:string "Interactive" :type :header-first}
+   {:string "Design & Development" :type :header-second}])
+
+(defn create-particles [{:keys [string type]}]
+  (mapv #(new-particle % type) string))
+
+(defonce populate-particle-list
+  (swap! app-state
+         update-in [:particle-list]
+         into (mapv create-particles string-list)))
+
+
 (defn handle-mouse-move [e data]
   (om/update! data :mouse {:x e.pageX :y e.pageY}))
-
-;(defn update-particle [data owner]
-  ;(let [el (om/get-node owner)
-        ;m {:el el
-           ;:width (.-offsetWidth el) :height (.-offsetHeight el)
-           ;:origin-x (-> el .getBoundingClientRect .-left)
-           ;:origin-y (-> el .getBoundingClientRect .-top)}]
-    ;(om/transact! data #(into % m))))
-
-;(defn particle-view [data owner]
-  ;(reify
-    ;om/IDidMount
-    ;(did-mount [_]
-      ;(update-particle data owner))
-    ;om/IRender
-    ;(render [_]
-      ;(sab/html
-        ;[:span (:char data)]))))
 
 (defn particle-view [data owner]
   (reify
     om/IDidMount
     (did-mount [_]
       (let [el (om/get-node owner)
-            m {:char data
-               :el el
+            p {:el el
                :width (.-offsetWidth el) :height (.-offsetHeight el)
                :origin-x (-> el .getBoundingClientRect .-left)
-               :origin-y (-> el .getBoundingClientRect .-top)}
-            m (conj m (new-particle))]
-        (om/transact! (particle-list) #(conj % m))))
+               :origin-y (-> el .getBoundingClientRect .-top)}]
+        (om/transact! data #(conj % p))))
     om/IRender
     (render [_]
       (sab/html
-        [:span data]))))
+        [:span (:string data)]))))
 
-;(defn particles-template [{:keys [particle-list]} type]
-  ;(map-indexed #(om/build particle-view %2 {:react-key %1})
-               ;(filter #(= (:type %) type) particle-list)))
 
-(defn particles [s]
-  (map-indexed #(om/build particle-view %2 {:react-key %1}) s))
+(defn build-particles [{:keys [particle-list]} type]
+  (map-indexed #(om/build particle-view %2 {:react-key %1})
+               (filter #(= (:type %) type) particle-list)))
 
 (defn app-view [data owner]
   (reify
@@ -94,11 +76,8 @@
       (sab/html
         [:div.board {:on-mouse-move #(handle-mouse-move % data)}
          [:div.headers
-          [:div.header (particles "Interactive")]
-          [:div.header (particles "Design & Development")]
-          ;[:div.header (particles-template data :header-first)]
-          ;[:div.header (particles-template data :header-second)]
-          ]
+          [:div.header (build-particles data :header-first)]
+          [:div.header (build-particles data :header-second)]]
          [:br]
          [:div.mcoords
           [:span "x:" (-> data :mouse :x)] " "
