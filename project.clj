3209ae38f0550ba9aad8e44c1f069fdd125862fe
Udschal Imanov <udschal.imanov@gmail.com>
2016-02-18 17:40:59 +0100

    upgrade deps

--- project.clj
+++ project.clj
@@ -7,16 +7,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.189"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [org.clojure/core.async "0.2.374"]
                  [reagent "0.5.1"]
-                 [re-frame "0.5.0"]
+                 [re-frame "0.6.0"]
                  [camel-snake-kebab "0.3.2"]
                  [bardo "0.1.1-SNAPSHOT"]
-                 [binaryage/devtools "0.4.1"]]
+                 [binaryage/devtools "0.5.2"]]
 
-  :plugins [[lein-cljsbuild "1.1.1"]
-            [lein-figwheel "0.5.0-2"]]
+  :plugins [[lein-cljsbuild "1.1.2"]
+            [lein-figwheel "0.5.0-6"]]
 
   :source-paths ["src"]
 
